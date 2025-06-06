6b6348576eb045335ac0fd2a581c521a522c7269
Juan <juancruzmur@gmail.com>
2025-06-06 13:20:45 -0300

    fix about me

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -29,8 +29,9 @@ export default function AboutMe() {
       <div className="w-full md:w-auto p-12 space-y-5 transition-all duration-300  hover:-translate-y-1">
         <h2 className="text-4xl text-center font-bold">{t("about.title")}</h2>
 
-        <p className=" text-gray-400 text-center">{t("about.p1")}</p>
-        <p className="text-gray-400 text-center">{t("about.p2")}</p>
+        <p className=" text-gray-400 text-center">
+          {t("about.p1")} {t("about.p2")}
+        </p>
       </div>
 
       <CardStack items={aboutMeFacts} />
